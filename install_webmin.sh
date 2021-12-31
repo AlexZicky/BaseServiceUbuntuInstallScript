d87aaf598716a6d747d73c8d80caee2d4d0db758
AlexZicky <AlexZicky@users.noreply.github.com>
2021-12-31 11:55:23 +0100

    Update install_webmin.sh

--- install_webmin.sh
+++ install_webmin.sh
@@ -3,8 +3,7 @@
 
 # install Webmin
 cd /root
-wget http://prdownloads.sourceforge.net/webadmin/webmin_1.791_all.deb
+wget https://prdownloads.sourceforge.net/webadmin/webmin_1.984_all.deb
 apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
 #apt-get -f install
-# dpkg --install webmin_1.831_all.deb
-dpkg --install webmin_1.791_all.deb
+dpkg --install webmin_1.984_all.deb
